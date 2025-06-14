32b76ca6dfe3aa6e64cd3c21a72a96b7fac2acce
Ethan Laloum <ethanlaloum@Host-002.lan>
2025-06-14 16:15:18 +0200

    [+] - Fill page.tsx

---
+++ app/(main)/privacy/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { ComingSoon } from "@/components/coming-soon"
+
+export default function PrivacyPage() {
+  return (
+    <ComingSoon
+      title="Privacy - Bientôt Disponible"
+      description="Ressources et informations pour les médias."
+      backLabel="Retour à l'Accueil"
+      backLink="/"
+    />
+  )
+}
---
+++ app/(main)/returns/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { ComingSoon } from "@/components/coming-soon"
+
+export default function ReturnsPage() {
+  return (
+    <ComingSoon
+      title="Returns - Bientôt Disponible"
+      description="Ressources et informations pour les médias."
+      backLabel="Retour à l'Accueil"
+      backLink="/"
+    />
+  )
+}
